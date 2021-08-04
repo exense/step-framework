2f72ff3ce910a6c17b87886f4684313bb4f6ed1d
David Stephan <David.Stephan@exense.ch>
2021-08-04 16:04:17 +0200

    CORS orgin read from configuration

--- step-framework-parent/step-framework-server/src/main/java/step/framework/server/CORSResponseFilter.java
+++ step-framework-parent/step-framework-server/src/main/java/step/framework/server/CORSResponseFilter.java
@@ -1,7 +1,10 @@
 package step.framework.server;
 
+import ch.exense.commons.app.Configuration;
+
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -9,12 +12,20 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class CORSResponseFilter implements ContainerResponseFilter {
+public class CORSResponseFilter extends AbstractServices implements ContainerResponseFilter {
+
+	private String origin;
+	
+	@PostConstruct
+	public void init() throws Exception {
+		Configuration configuration = getServerContext().getConfiguration();
+		origin = configuration.getProperty("frontend.baseUrl", "*");
+	}
 
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
 			throws IOException {
 		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-		headers.add("Access-Control-Allow-Origin", "http://localhost:4200");
+		headers.add("Access-Control-Allow-Origin", origin);
 		headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 		headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, origin, accept, authorization");
 		headers.add("Access-Control-Allow-Credentials","true");
