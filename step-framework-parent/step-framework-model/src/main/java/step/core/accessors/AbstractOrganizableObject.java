28f97fe492b8ff43152e8f934307547c9eeb5423
David Stephan <David.Stephan@exense.ch>
2022-12-22 11:38:52 +0100

    SED-1632 reimplement entity migration service using bulk operation manager (#15)
    * SED-1632 Implementing bulk operations in TableService
    
    * Revert "SED-496 adding @JsonIgnore"
    
    This reverts commit 5ad6890c179108e5eebcec1f575b166131b89a38.
    
    * Revert "Adding debug logs"
    
    This reverts commit 8eab27ec4b79b892ed6f34c00bece322a047524a.
    
    Co-authored-by: Jérôme Comte <jerome.comte@exense.ch>

--- step-framework-parent/step-framework-model/src/main/java/step/core/accessors/AbstractOrganizableObject.java
+++ step-framework-parent/step-framework-model/src/main/java/step/core/accessors/AbstractOrganizableObject.java
@@ -18,8 +18,6 @@
  ******************************************************************************/
 package step.core.accessors;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,10 +30,10 @@ import java.util.Map;
 public class AbstractOrganizableObject extends AbstractIdentifiableObject {
 	
 	protected Map<String, String> attributes;
-
+	
 	public static final String NAME = "name";
 	public static final String VERSION = "version";
-
+	
 	public AbstractOrganizableObject() {
 		super();
 	}
@@ -46,36 +44,6 @@ public class AbstractOrganizableObject extends AbstractIdentifiableObject {
 	public Map<String, String> getAttributes() {
 		return attributes;
 	}
-
-	/**
-	 * @return the name of this object's instance
-	 */
-	@JsonIgnore
-	public String getName() {
-		return getAttribute(NAME);
-	}
-
-	/**
-	 * @return the version of this object's instance
-	 */
-	public String getVersion() {
-		return getAttribute(VERSION);
-	}
-
-	/**
-	 * set the name of this object's instance
-	 */
-	public void setName(String name) {
-		addAttribute(NAME,name);
-	}
-
-	/**
-	 * set the version of this object's instance
-	 */
-	public void setVersion(String version) {
-		addAttribute(VERSION,version);
-	}
-
 
 	/**
 	 * Sets the attributes used to identify this object's instance
