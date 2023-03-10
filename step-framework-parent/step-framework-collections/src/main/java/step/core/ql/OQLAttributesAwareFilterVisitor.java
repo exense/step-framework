ab8076069ea295fc8cf229de1fd8f2efd02f6cf9
Jérôme Comte <jerome.comte@exense.ch>
2023-03-10 13:27:43 +0100

    SED-1306 Implementing suggestion for discussion_r1131744956

--- step-framework-parent/step-framework-collections/src/main/java/step/core/ql/OQLAttributesAwareFilterVisitor.java
+++ step-framework-parent/step-framework-collections/src/main/java/step/core/ql/OQLAttributesAwareFilterVisitor.java
@@ -8,13 +8,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
-public class OQLAttributesAwareFilterVisitor extends OQLBaseVisitor<Filter> {
+public class OQLAttributesAwareFilterVisitor extends OQLFilterVisitor {
 
-    private List<String> attributes = new ArrayList<>();
-    private Function<String, String> attributesTransformFunction;
-    private Collection<String> ignoreAttributes;
+    private final List<String> attributes = new ArrayList<>();
+    private final Function<String, String> attributesTransformFunction;
+    private final Collection<String> ignoreAttributes;
 
     public OQLAttributesAwareFilterVisitor() {
         this(Function.identity(), Collections.emptySet());
@@ -22,19 +21,14 @@ public class OQLAttributesAwareFilterVisitor extends OQLBaseVisitor<Filter> {
 
     /**
      * Transform is the first operation, then ignore attributes
+     *
      * @param attributesTransformFunction
      * @param ignoreAttributes
      */
     public OQLAttributesAwareFilterVisitor(Function<String, String> attributesTransformFunction, Collection<String> ignoreAttributes) {
+        super();
         this.attributesTransformFunction = attributesTransformFunction != null ? attributesTransformFunction : Function.identity();
         this.ignoreAttributes = ignoreAttributes;
-    }
-
-    @Override
-    public Filter visitAndExpr(OQLParser.AndExprContext ctx) {
-        final Filter left = this.visit(ctx.expr(0));
-        final Filter right = this.visit(ctx.expr(1));
-        return Filters.and(List.of(left, right));
     }
 
     @Override
@@ -43,17 +37,12 @@ public class OQLAttributesAwareFilterVisitor extends OQLBaseVisitor<Filter> {
         if (ignoreAttributes.contains(text0)) {
             return Filters.empty();
         }
+        trackAttribute(text0);
+        return super.visitEqualityExpr(ctx);
+    }
+
+    private void trackAttribute(String text0) {
         attributes.add(text0);
-        String text1 = unescapeStringIfNecessary(ctx.expr(1).getText());
-        if (ctx.EQ() != null) {
-            return Filters.equals(text0, text1);
-        } else if (ctx.NEQ() != null) {
-            return Filters.not(Filters.equals(text0, text1));
-        } else if (ctx.REGEX() != null) {
-            return Filters.regex(text0, text1,false);
-        } else {
-            throw new UnsupportedOperationException("Operation of the provided equality expression is not supported. Expression: " + ctx.getText());
-        }
     }
 
     @Override
@@ -62,39 +51,8 @@ public class OQLAttributesAwareFilterVisitor extends OQLBaseVisitor<Filter> {
         if (ignoreAttributes.contains(text0)) {
             return Filters.empty();
         }
-        attributes.add(text0);
-        String text1 = unescapeStringIfNecessary(ctx.expr(1).getText());
-        Long value;
-        try {
-            value = Long.parseLong(text1);
-        } catch (Exception e) {
-            throw new UnsupportedOperationException("Comparison expression only support long value. Expression: " + ctx.getText());
-        }
-        if (ctx.LT() != null) {
-            return Filters.lt(text0, value);
-        } else if (ctx.LTE() != null) {
-            return Filters.lte(text0, value);
-        } else if (ctx.GT() != null) {
-            return Filters.gt(text0, value);
-        } else if (ctx.GTE() != null) {
-            return Filters.gte(text0, value);
-        } else {
-            throw new UnsupportedOperationException("Operation of the provided comparison expression is not supported. Expression: " + ctx.getText());
-        }
-    }
-
-    protected String unescapeStringIfNecessary(String text1) {
-        if(text1.startsWith("\"") && text1.endsWith("\"")) {
-            text1 = unescapeStringAtom(text1);
-        }
-        return text1;
-    }
-
-    @Override
-    public Filter visitOrExpr(OQLParser.OrExprContext ctx) {
-        final Filter left = this.visit(ctx.expr(0));
-        final Filter right = this.visit(ctx.expr(1));
-        return Filters.or(List.of(left, right));
+        trackAttribute(text0);
+        return super.visitComparisonExpr(ctx);
     }
 
     @Override
@@ -103,37 +61,8 @@ public class OQLAttributesAwareFilterVisitor extends OQLBaseVisitor<Filter> {
         if (ignoreAttributes.contains(text0)) {
             return Filters.empty();
         }
-        attributes.add(text0);
-        List<String> ins = ctx.STRING().stream().map(tn -> unescapeStringIfNecessary(tn.getText())).collect(Collectors.toList());
-        return Filters.in(text0, ins);
-    }
-
-    @Override
-    public Filter visitNotExpr(OQLParser.NotExprContext ctx) {
-        final Filter expr = this.visit(ctx.expr());
-        return Filters.not(expr);
-    }
-
-    @Override
-    public Filter visitParExpr(OQLParser.ParExprContext ctx) {
-        return this.visit(ctx.expr());
-    }
-
-    @Override
-    public Filter visitNonQuotedStringAtom(OQLParser.NonQuotedStringAtomContext ctx) {
-        return Filters.fulltext(ctx.getText());
-    }
-
-    @Override
-    public Filter visitStringAtom(OQLParser.StringAtomContext ctx) {
-        String str = unescapeStringAtom(ctx.getText());
-        return Filters.fulltext(str);
-    }
-
-    protected String unescapeStringAtom(String str) {
-        // strip quotes
-        str = str.substring(1, str.length() - 1).replace("\"\"", "\"");
-        return str;
+        trackAttribute(text0);
+        return super.visitInExpr(ctx);
     }
 
     public List<String> getAttributes() {
