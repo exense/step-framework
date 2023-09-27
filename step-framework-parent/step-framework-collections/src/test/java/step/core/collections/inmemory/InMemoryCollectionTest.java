4b8e51b175734ba60fba4de05baa146a1e0e17ff
Christoph Langguth <christoph.langguth@exense.ch>
2023-09-27 11:54:06 +0200

    SED-2444 Implement drop for InMemoryCollection

--- step-framework-parent/step-framework-collections/src/test/java/step/core/collections/inmemory/InMemoryCollectionTest.java
+++ step-framework-parent/step-framework-collections/src/test/java/step/core/collections/inmemory/InMemoryCollectionTest.java
@@ -1,13 +1,25 @@
 package step.core.collections.inmemory;
 
-import java.util.Properties;
-
+import org.junit.Assert;
+import org.junit.Test;
 import step.core.collections.AbstractCollectionTest;
+import step.core.collections.Document;
+
+import java.util.Properties;
 
 public class InMemoryCollectionTest extends AbstractCollectionTest {
 
-	public InMemoryCollectionTest() {
-		super(new InMemoryCollectionFactory(new Properties()));
-	}
+    public InMemoryCollectionTest() {
+        super(new InMemoryCollectionFactory(new Properties()));
+    }
 
+    @Test
+    public void testDrop() {
+        InMemoryCollection<Document> c = new InMemoryCollection<>();
+        c.save(new Document());
+        c.save(new Document());
+        Assert.assertEquals(2, c.estimatedCount());
+        c.drop();
+        Assert.assertEquals(0, c.estimatedCount());
+    }
 }
