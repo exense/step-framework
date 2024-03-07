e8242ffa8945629410e736da4a95eabfa2035190
David Stephan <david.stephan@exense.ch>
2024-03-07 16:54:51 +0100

    SED-2939 move new SessionFilter to framework

---
+++ step-framework-parent/step-framework-server/src/main/java/step/framework/server/security/SessionFilter.java
@@ -0,0 +1,35 @@
+package step.framework.server.security;
+
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.ext.Provider;
+import step.framework.server.AbstractServices;
+import step.framework.server.Session;
+import step.framework.server.access.TokenType;
+
+import java.io.IOException;
+
+
+/**
+ * First filter to be executed, initialize Session when required
+ */
+@Provider
+@Priority(1) //Run first
+public class SessionFilter extends AbstractServices implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+        retrieveOrInitializeSession();
+    }
+
+    protected Session retrieveOrInitializeSession() {
+        Session session = getSession();
+        if(session == null) {
+            session = new Session();
+            session.setAuthenticated(false); //no authentication here, only setting
+            session.setTokenType(TokenType.LOCAL_UI_TOKEN);//default
+            setSession(session);
+        }
+        return session;
+    }
+}
