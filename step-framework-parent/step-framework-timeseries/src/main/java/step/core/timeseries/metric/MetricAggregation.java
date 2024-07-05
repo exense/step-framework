10b29b9887079f37363c5bf8356c642fe860f2b0
Lucian Baciu <lucian.baciu@exense.ch>
2024-07-06 00:40:36 +0300

    SED-3164-changed aggregate structure

--- step-framework-parent/step-framework-timeseries/src/main/java/step/core/timeseries/metric/MetricAggregation.java
+++ step-framework-parent/step-framework-timeseries/src/main/java/step/core/timeseries/metric/MetricAggregation.java
@@ -1,12 +1,40 @@
 package step.core.timeseries.metric;
 
-public enum MetricAggregation {
-    SUM,
-    AVG,
-    MAX,
-    MIN,
-    COUNT,
-    RATE,
-    MEDIAN,
-    PERCENTILE
+import java.util.Map;
+
+
+
+public class MetricAggregation {
+    private MetricAggregationType type;
+    private Map<String, Object> params;
+
+    public MetricAggregation() {
+    }
+    
+    public MetricAggregation(MetricAggregationType type) {
+        this.type = type;
+    }
+
+    public MetricAggregation(MetricAggregationType type, Map<String, Object> params) {
+        this.type = type;
+        this.params = params;
+    }
+
+    public MetricAggregationType getType() {
+        return type;
+    }
+
+    public MetricAggregation setType(MetricAggregationType type) {
+        this.type = type;
+        return this;
+    }
+
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public MetricAggregation setParams(Map<String, Object> params) {
+        this.params = params;
+        return this;
+    }
 }
