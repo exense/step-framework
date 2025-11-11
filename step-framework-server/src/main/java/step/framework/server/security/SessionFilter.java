f65d95a30b4e8da8ff3d2e5a69e2f8f7a3172a2c
David Stephan <david.stephan@exense.ch>
2025-11-11 16:49:36 +0100

    SED-4356 High number of HTTP sessions created by live reporting

--- step-framework-server/src/main/java/step/framework/server/security/SessionFilter.java
+++ step-framework-server/src/main/java/step/framework/server/security/SessionFilter.java
@@ -1,9 +1,11 @@
 package step.framework.server.security;
 
 import jakarta.annotation.Priority;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.ext.Provider;
+import org.glassfish.jersey.server.ExtendedUriInfo;
 import step.framework.server.AbstractServices;
 import step.framework.server.Session;
 import step.framework.server.access.TokenType;
@@ -17,9 +19,15 @@ import java.io.IOException;
 @Provider
 @Priority(1) //Run first
 public class SessionFilter extends AbstractServices implements ContainerRequestFilter {
+    @Inject
+    private ExtendedUriInfo extendendUriInfo;
+
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-        retrieveOrInitializeSession();
+        NoSession annotation = extendendUriInfo.getMatchedResourceMethod().getInvocable().getHandlingMethod().getAnnotation(NoSession.class);
+        if (annotation == null) {
+            retrieveOrInitializeSession();
+        }
     }
 
     protected Session retrieveOrInitializeSession() {
